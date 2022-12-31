791533d887c5fd27e9a7b43af4aec5b764816afb
Anoop Singh <95135696+anoop7384@users.noreply.github.com>
2022-12-31 23:23:03 +0530

    Update urls.py

--- diva/urls.py
+++ diva/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('', include('base.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL,
