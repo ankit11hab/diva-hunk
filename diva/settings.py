6b17f093dea135f84e6b3656b770d4ac55338b2c
unknown <ankit11hab@gmail.com>
2023-01-01 17:58:47 +0530

    deploy

--- diva/settings.py
+++ diva/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ENTER THE EMAIL'
-EMAIL_HOST_PASSWORD = 'ENTER THE PASSWORD'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
