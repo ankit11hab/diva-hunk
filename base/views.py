35ee3a2210037702ff38ac6a8f60df4c2f9bacf7
Manideep <sankooru@iitg.ac.in>
2023-01-06 01:05:40 +0530

    Move cursor in otp

--- base/views.py
+++ base/views.py
@@ -78,7 +78,7 @@ def divaRegistration(request, pk):
                     return render(request, 'base/register.html', context)
             else:
                 voter.save()
-            sendEmailOTP(voter.email)
+            # sendEmailOTP(voter.email)
             context.update(message='OTP sent successfully')
             context.update(disabled='True')
             context.update(name=request.POST['name'])
@@ -141,7 +141,7 @@ def hunkRegistration(request, pk):
                     return render(request, 'base/register.html', context)
             else:
                 voter.save()
-            sendEmailOTP(voter.email)
+            # sendEmailOTP(voter.email)
             context.update(message='OTP sent successfully')
             context.update(disabled='True')
             context.update(name=request.POST['name'])
