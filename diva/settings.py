78c2efd1f5c4aef3d1c758d6bbe8b2b0426ca45f
ankit11hab <ankit11hab@gmail.com>
2023-01-21 21:10:35 +0530

    db

--- diva/settings.py
+++ diva/settings.py
@@ -75,13 +75,30 @@ WSGI_APPLICATION = 'diva.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       
+
+       'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': os.environ.get('db_name'),
+
+        'USER': os.environ.get('db_user'),
+
+        'PASSWORD': os.environ.get('db_password'),
+
+        'HOST': os.environ.get('db_host'),
+
+        'PORT': os.environ.get('db_port')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
